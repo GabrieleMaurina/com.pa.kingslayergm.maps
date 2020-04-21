2b07f9de43216384d9c62b336a21fc3f0da96539
GabrieleMaurina <gabrielemaurina95@gmail.com>
2020-04-21 20:00:57 +0200

    Added 3 maps.

--- ui/mods/kingslayergm/maps.js
+++ ui/mods/kingslayergm/maps.js
@@ -1,5 +1,7 @@
 cShareSystems.load_pas("King's Map Pack", [
     "coui://ui/mods/kingslayergm/systems/1v1_or_2v2_or_3v3_or_4v4_or_5v5_desertrocks.pas",
+    "coui://ui/mods/kingslayergm/systems/1v1v1_or_2v2v2_or_3v3v3_or_9_ffa_tripleplanet.pas",
+    "coui://ui/mods/kingslayergm/systems/2v2_ or_3v3_or_4v4_waterworld.pas",
     "coui://ui/mods/kingslayergm/systems/2v2_or_3v3_or_4v4_icymountains.pas",
     "coui://ui/mods/kingslayergm/systems/2v2_or_3v3_or_4v4_junglewar.pas",
     "coui://ui/mods/kingslayergm/systems/2v2v2_or_3v3_or_6_ffa_corridor.pas",
@@ -10,6 +12,7 @@ cShareSystems.load_pas("King's Map Pack", [
     "coui://ui/mods/kingslayergm/systems/2v2v2v2_or_4v4_or_8_ffa_bigxes.pas",
     "coui://ui/mods/kingslayergm/systems/2v2v2v2_or_4v4_or_8_ffa_or_14_ffa_holes.pas",
     "coui://ui/mods/kingslayergm/systems/2v2v2v2_or_4v4_or_8_ffa_temples.pas",
+    "coui://ui/mods/kingslayergm/systems/2v2v2v2_or_4v4_or_8_ffa_waterwar.pas",
     "coui://ui/mods/kingslayergm/systems/2v2v2v2v2_or_10_ffa_volcanos.pas",
     "coui://ui/mods/kingslayergm/systems/2v2v2v2v2_planetswar.pas"
 ]);
