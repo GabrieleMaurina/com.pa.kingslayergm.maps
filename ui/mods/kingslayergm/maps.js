79c459a7728c93934a7fc2fda9e8db4046ff63ff
GabrieleMaurina <gabrielemaurina95@gmail.com>
2020-04-25 16:14:53 +0200

    Added images.

--- ui/mods/kingslayergm/maps.js
+++ ui/mods/kingslayergm/maps.js
@@ -1,5 +1,6 @@
 cShareSystems.load_pas("King's Map Pack", [
     "coui://ui/mods/kingslayergm/systems/1v1_or_2v2_or_3v3_or_4v4_or_5v5_desertrocks.pas",
+    "coui://ui/mods/kingslayergm/systems/1v1v1_or_2v2v2_mountainranges.pas",
     "coui://ui/mods/kingslayergm/systems/1v1v1_or_2v2v2_or_3v3v3_or_9_ffa_tripleplanet.pas",
     "coui://ui/mods/kingslayergm/systems/2v2_ or_3v3_or_4v4_waterworld.pas",
     "coui://ui/mods/kingslayergm/systems/2v2_or_3v3_or_4v4_icymountains.pas",
